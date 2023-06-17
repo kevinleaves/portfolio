b8cd94e7c4752f6a9b8849697759e8964d170194
Kevin Le <iamkevinvle@gmail.com>
2023-06-16 23:14:22 -0700

    overhaul project descriptions

--- src/app/modules/work/projectsData.js
+++ src/app/modules/work/projectsData.js
@@ -1,20 +1,27 @@
 const projects = [
   {
-    id: 3,
+    id: 4,
     name: 'Soundclash',
     role: 'Software Engineer',
     description:
       'Utilizing the Spotify API, pit your top songs against each other in an elimination style tournament and find your favorite song amongst your favorites.',
-    stack: ['Typescript', 'TailwindCSS', 'Node.js', 'Express.js'],
+    stack: [
+      'React',
+      'Typescript',
+      'Spotify API',
+      'Express.js',
+      'Oauth 2.0',
+      'TailwindCSS',
+    ],
     link: 'https://github.com/kevinleaves/Soundclash',
     image: 'soundclash.png',
   },
   {
-    id: 4,
+    id: 3,
     name: 'Harper',
     role: 'Software Engineer',
     description:
-      'A streaming platform where artists can connect with their fans.',
+      'A content management platform designed for artists, empowering them to showcase their work effectively. Artists have full control over the featured video, can display their music videos, and share upcoming streams and tour dates, fostering direct connections with their fans.',
     stack: ['React', 'Redux', 'TailwindCSS', 'Firebase', 'Express.js'],
     link: 'https://github.com/kevinleaves/Harper-Woodward',
     image: 'harper_woodward.gif',
@@ -23,7 +30,8 @@ const projects = [
     id: 1,
     name: 'Mecca',
     role: 'Software Engineer',
-    description: 'A modern redesign of an e-commerce website.',
+    description:
+      'A modern redesign of an e-commerce website. Accessed existing product data through RESTful API endpoints to display client product information. Created resusable, responsive components that adapt to various screen sizes.',
     stack: ['React', 'Javascript', 'CSS', 'Node.js', 'Express.js'],
     link: 'https://github.com/RFP-Coco/Mecca',
     image: 'Mecca.png',
@@ -33,7 +41,7 @@ const projects = [
     name: 'Atelier',
     role: 'Software Engineer',
     description:
-      'Deployed an RESTful API microservice capable of handling real-world traffic.',
+      "Deployed an RESTful API microservice capable of handling real-world traffic. Performed ETL process on 15 million+ rows of CSV data, loaded data into a deployed Amazon EC2 instance, then stress tested using Loader.io to validate the system's resilience under peak loads. Optimized by using NGINX to horizonally scale to improve response times and user experience.",
     stack: [
       'PostgreSQL',
       'Redis',
@@ -51,17 +59,20 @@ const projects = [
     id: 5,
     name: 'Pomoquest',
     role: 'Software Engineer',
-    description: 'A gamified pomodoro timer to improve productivity',
+    description:
+      'A production-ready, gamified pomodoro timer to improve productivity. Upon completion of any of the timers, user gains coins which you can use to improve the in-app experience. Deployed on Vercel & PlanetScale',
     stack: [
       'React',
       'TypeScript',
       'Next.js',
+      'Clerk Auth',
       'TRPC',
-      'Prisma',
+      'Prisma ORM',
       'Material UI',
       'TailwindCSS',
     ],
     link: 'https://github.com/kevinleaves/pomoquest',
+    link2: 'https://www.pomoquest.vercel.app',
     image: 'pomoquest.png',
   },
 ]
