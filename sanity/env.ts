32b157373ecc117330a7c74b626a2d81c30c80ff
Kevin Le <iamkevinvle@gmail.com>
2023-07-23 19:04:34 -0700

    add alternative env prefixes

--- sanity/env.ts
+++ sanity/env.ts
@@ -11,6 +11,18 @@ export const projectId = assertValue(
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
+// export const apiVersion = process.env.SANITY_API_VERSION || '2023-07-23'
+
+// export const dataset = assertValue(
+//   process.env.SANITY_STUDIO_DATASET,
+//   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
+// )
+
+// export const projectId = assertValue(
+//   process.env.SANITY_STUDIO_PROJECT_ID1,
+//   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+// )
+
 export const useCdn = false
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
