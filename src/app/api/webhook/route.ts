20059ab322d7894a6dd692d556ed6b59fef835c1
Kevin Le <iamkevinvle@gmail.com>
2023-07-27 14:17:05 -0700

    use fetched contact list as email recipients

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,14 +1,21 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { convertToHTML } from './utils/convertToHTML'
-import SendGrid from 'src/app/_sendgrid/SendGrid'
-import { Post } from 'types/interfaces'
+import { SendGrid, getContactList } from 'src/app/_sendgrid/SendGrid'
+import { Post, GetListResponse, Contact } from 'types/interfaces'
 
 export async function POST(request: NextRequest) {
   const body: Post = await request.json()
   console.log(body)
   const { _id, title, body: content } = body
   const htmlString = convertToHTML(content)
-  console.log(convertToHTML(content), 'htmlstring')
-  const res = await SendGrid(title, htmlString)
+  // console.log(convertToHTML(content), 'htmlstring')
+  const response = await getContactList()
+  // console.log(response, 'response')
+  const recipients = response.contact_sample.map((contact) => {
+    const { email, first_name, last_name } = contact
+    return { email, name: `${first_name} ${last_name}` }
+  })
+  const res = await SendGrid(title, htmlString, recipients)
+
   return NextResponse.json(body)
 }
