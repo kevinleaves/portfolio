325cdd733770673a45365c28d7e6dac68db080f2
Kevin Le <iamkevinvle@gmail.com>
2023-07-27 14:16:18 -0700

    fetch max 50 contacts from sendgrid contact list

--- src/app/_sendgrid/SendGrid.ts
+++ src/app/_sendgrid/SendGrid.ts
@@ -1,3 +1,5 @@
+import { Post, GetListResponse, Contact } from 'types/interfaces'
+
 const url = 'https://api.sendgrid.com/v3/mail/send'
 
 const SENDGRID_API_KEY = process.env.NEXT_PUBLIC_SENDGRID_API_KEY
@@ -9,20 +11,24 @@ const SEND_TO = process.env.NEXT_PUBLIC_SEND_TO_EMAIL
  * takes in a richtext converted into htmlstring and fires request to sendgrid mailsend api
  * @param subject is the email's subject
  * @param content is an HTML string containing the body of the email
+ * @param recipients an array of recipients
  * @returns a promise
  */
 
-export default async function SendGrid(subject: string, content: string) {
-  console.log(content)
+interface Recipient {
+  email: string
+  name: string
+}
+
+export async function SendGrid(
+  subject: string,
+  content: string,
+  recipients: Recipient[]
+) {
   const body = {
     personalizations: [
       {
-        to: [
-          {
-            email: SEND_TO,
-            name: 'Kevin Le',
-          },
-        ],
+        to: recipients,
         subject,
       },
     ],
@@ -52,4 +58,20 @@ export default async function SendGrid(subject: string, content: string) {
   }
 
   return fetch(url, options)
+}
+
+export async function getContactList() {
+  const listID = process.env.NEXT_PUBLIC_SENDGRID_NEWSLETTER_LIST_ID
+  const url = `https://api.sendgrid.com/v3/marketing/lists/${listID}?contact_sample=true`
+
+  const options = {
+    method: 'GET',
+    headers: {
+      authorization: `Bearer ${SENDGRID_API_KEY}`,
+      'Content-Type': 'application/json',
+    },
+  }
+
+  const contacts: Promise<GetListResponse> = (await fetch(url, options)).json()
+  return contacts
 }
