3a885b792ae4a8a90c91616bf444e8491b54175e
Kevin Le <iamkevinvle@gmail.com>
2023-07-28 16:45:46 -0700

    frontend styling

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -17,16 +17,16 @@ export default async function BlogPage() {
 
   return (
     <main className='flex flex-col'>
-      <h1>blog page</h1>
+      <h1 className='mb-12 text-center text-6xl font-bold leading-tight tracking-tighter md:text-left md:text-7xl md:leading-none lg:text-8xl'>
+        my musings about life, tech, & more
+      </h1>
+      <Posts posts={posts} />
       <div className='flex h-screen w-full flex-col'>
         <iframe
           className='h-full w-full'
           src='https://cdn.forms-content-1.sg-form.com/ba9eebc9-2cb0-11ee-b588-9eb060adcbeb'
         />
       </div>
-      <div>{JSON.stringify(post)}</div>
-      <div>{JSON.stringify(morePosts)}</div>
-      <Posts posts={posts} />
     </main>
   )
 }
