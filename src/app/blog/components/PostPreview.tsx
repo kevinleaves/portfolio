628d04dfa43328797c5cf119daf66abc4f1be8e5
Kevin Le <iamkevinvle@gmail.com>
2023-08-26 12:40:52 -0700

    remove excerpt from post preview

--- src/app/blog/components/PostPreview.tsx
+++ src/app/blog/components/PostPreview.tsx
@@ -1,13 +1,19 @@
-import CoverImage from "./CoverImage"
-import { Post } from "types/interfaces"
-import Link from "next/link"
-import PostDate from "./PostDate"
-import AuthorImage from "./AvatarImage"
+import CoverImage from './CoverImage'
+import { Post } from 'types/interfaces'
+import Link from 'next/link'
+import PostDate from './PostDate'
+import AuthorAvatar from './AuthorAvatar'
 
-export default function PostPreview ({title, slug, author, mainImage, publishedAt}: Omit<Post, '_id' | 'body' | '_type' | 'authorName'>) {
+export default function PostPreview({
+  title,
+  slug,
+  author,
+  mainImage,
+  publishedAt,
+}: Omit<Post, '_id' | 'body' | '_type' | 'authorName'>) {
   return (
     <div>
-      <div className="mb-5">
+      <div className='mb-5'>
         <CoverImage
           slug={slug}
           title={title}
@@ -15,18 +21,13 @@ export default function PostPreview ({title, slug, author, mainImage, publishedA
           priority={false}
         />
       </div>
-      <h3 className="mb-3 text-3xl leading-snug">
-        <Link href={`/blog/posts/${slug}`} className="hover:underline">
+      <h3 className='mb-3 text-xl leading-snug'>
+        <Link href={`/blog/posts/${slug}`} className='hover:underline'>
           {title}
         </Link>
       </h3>
-      <div className="mb-4 text-lg">
-        <PostDate dateString={publishedAt} />
-      </div>
-      {/* {excerpt && <p className="mb-4 text-lg leading-relaxed">{excerpt}</p>} */}
-      {/* {author && <Avatar name={author.name} picture={author.picture} />} */}
-      {/* <span>{JSON.stringify(author)}</span> */}
-      <AuthorImage name={author.name} image={author.image}/>
+      <PostDate dateString={publishedAt} />
+      <AuthorAvatar name={author.name} image={author.image} />
     </div>
   )
 }
