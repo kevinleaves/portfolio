79ffbc6648c90d0d155080c26601b4c28bd4fbf9
Kevin Le <iamkevinvle@gmail.com>
2023-08-28 13:56:07 -0700

    add node interface to Code component

--- src/app/blog/components/Code.tsx
+++ src/app/blog/components/Code.tsx
@@ -1,9 +1,19 @@
 'use client'
+
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
-export default function Code({ node }) {
-  console.log(node, 'node')
+interface Props {
+  node: {
+    _type: 'code'
+    language: string
+    highlightedLines: Array<Number>
+    code: string
+    filename?: string
+  }
+}
+
+export default function Code({ node }: Props) {
   if (!node || !node.code) {
     return null
   }
