72d7edf983f417d20675951a9f823d151edddf63
Kevin Le <iamkevinvle@gmail.com>
2023-09-19 15:45:28 -0700

    install vercel analytics tracker

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Providers } from './providers'
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata = {
   icons: {
@@ -17,7 +18,10 @@ export default function RootLayout({
   return (
     <html lang='en' className='dark'>
       <body className='flex min-h-screen min-w-[320px] flex-col bg-lightPrimary dark:bg-darkPrimary dark:text-lightPrimary'>
-        <Providers>{children}</Providers>
+        <Providers>
+          {children}
+          <Analytics />
+        </Providers>
       </body>
     </html>
   )
