50e53b5feef2a5b128f9521a60daa1a8d6a9b032
Kevin Le <iamkevinvle@gmail.com>
2023-10-20 18:34:46 -0700

    overhaul home/hero section

--- src/app/modules/home/Home.tsx
+++ src/app/modules/home/Home.tsx
@@ -1,81 +1,19 @@
-import React, { useEffect, useReducer, RefObject } from 'react'
-
-const titles = [
-  'software engineer',
-  'fullstack engineer',
-  'frontend engineer',
-  'backend engineer',
-]
-
-type State = {
-  index: number
-}
-
-type Action = { type: 'increment' } | { type: 'reset' }
-
-const reducer = (state: State, action: Action) => {
-  switch (action.type) {
-    case 'increment':
-      return {
-        ...state,
-        index: state.index + 1,
-      }
-    case 'reset':
-      return {
-        ...state,
-        index: 0,
-      }
-    default:
-      return state
-  }
-}
+import React, { RefObject } from 'react'
 
 type Props = {
   homeRef: RefObject<HTMLElement>
 }
 
 export default function Home({ homeRef }: Props): JSX.Element {
-  const initialState = { index: 0 }
-  const [state, dispatch] = useReducer(reducer, initialState)
-
-  useEffect(() => {
-    const changeTitle = () => {
-      setTimeout(() => {
-        dispatch({ type: 'increment' })
-      }, 3000)
-
-      if (state.index >= titles.length) {
-        dispatch({ type: 'reset' })
-      }
-    }
-    changeTitle()
-  }, [state.index])
-
-  const title = titles[state.index]
-
   return (
-    <section
-      ref={homeRef}
-      className='grid h-[50vh] w-[100vw] place-content-center gap-2 text-3xl md:h-[100vh]'
-      id='home'
-    >
-      <div className='place-items-center gap-2 '>
-        <h1 className='items-start gap-2'>
-          hi, my name is
-          <p className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-5xl font-extrabold text-transparent'>
-            Kevin Le.
-          </p>
-          and i am a
-        </h1>
-        <div className='w-60'>
-          <h3
-            key={title}
-            className='z-0 animate-pulsemore bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-start text-transparent transition duration-1000 ease-in-out'
-          >
-            {title}
-          </h3>
-        </div>
-      </div>
+    <section ref={homeRef} className='p-8 md:p-24' id='home'>
+      <h1 className='text-2xl font-semibold md:text-3xl'>
+        Hi, my name is
+        <p className='text-5xl font-extrabold text-blue-300 text-transparent'>
+          Kevin Le.
+        </p>
+        I'm a full stack developer who loves to transform ideas into code.
+      </h1>
     </section>
   )
 }
