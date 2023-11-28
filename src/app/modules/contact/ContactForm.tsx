b740206a7f8d813f32977636d633afab600b8d5b
Kevin Le <iamkevinvle@gmail.com>
2023-11-28 15:23:17 -0800

    add web3forms to send emails

--- src/app/modules/contact/ContactForm.tsx
+++ src/app/modules/contact/ContactForm.tsx
@@ -1,14 +1,16 @@
 'use client'
 import { useState } from 'react'
-import { useForm, SubmitHandler, Controller } from 'react-hook-form'
+import { useForm, Controller } from 'react-hook-form'
 import {
   Input,
   FormControl,
   FormErrorMessage,
   Textarea,
   FormLabel,
+  Spinner,
 } from '@chakra-ui/react'
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
+import useWeb3Forms from '@web3forms/react'
 
 type Inputs = {
   name: string
@@ -22,109 +24,125 @@ export default function Contact({}: Props) {
   const {
     control,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitSuccessful, isSubmitting },
     reset,
   } = useForm<Inputs>()
 
-  const [formClicked, setFormClicked] = useState(false)
+  const [isSuccess, setIsSuccess] = useState(false)
+  const [message, setMessage] = useState('')
 
-  const onSubmit: SubmitHandler<Inputs> = async (data) => {
-    setFormClicked(true)
-    // console.log('button clicked')
-    reset()
-  }
+  const apiKey =
+    process.env.NEXT_PUBLIC_PUBLIC_ACCESS_KEY || 'YOUR_ACCESS_KEY_HERE'
+
+  const { submit: onSubmit } = useWeb3Forms({
+    access_key: apiKey,
+    settings: {
+      from_name: `Kevin's Porfolio`,
+      subject: 'New Contact Message from your Portfolio website',
+    },
+    onSuccess: () => {
+      setIsSuccess(true)
+      setMessage('Thank you for submitting! You will be contacted shortly.')
+      reset()
+    },
+    onError: () => {
+      setIsSuccess(false)
+      setMessage('Something went wrong. Please try later.')
+    },
+  })
 
   return (
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className='flex w-full flex-col justify-center gap-12'
-    >
-      {/* register your input into the hook by invoking the "register" function */}
-      <Controller
-        name='name'
-        control={control}
-        rules={{ required: true }}
-        defaultValue=''
-        render={({ field }) => (
-          <FormControl isRequired isInvalid={errors.name ? true : false}>
-            <Input
-              {...field}
-              variant='outline'
-              placeholder='Name'
-              className='peer rounded-lg bg-slate-100 placeholder-transparent'
-            />
-            <FormLabel className='peer-focus:text-gray absolute -top-8 left-2 text-gray-400 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-8'>
-              Full Name
-            </FormLabel>
-            <FormErrorMessage>Please enter your name.</FormErrorMessage>
-          </FormControl>
-        )}
-      />
-      <Controller
-        name='email'
-        control={control}
-        defaultValue=''
-        rules={{
-          required: { value: true, message: 'Email Address is required.' },
-          pattern: {
-            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-            message: 'Please input a valid email address.',
-          },
-        }}
-        render={({ field }) => (
-          <FormControl isRequired isInvalid={errors.email ? true : false}>
-            <Input
-              {...field}
-              variant='outline'
-              isInvalid={errors.email ? true : false}
-              errorBorderColor='crimson'
-              placeholder='Email Address'
-              className='peer rounded-lg bg-slate-100 placeholder-transparent'
-            />
-            <FormLabel className='absolute -top-8 left-2 text-gray-400 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:left-2 peer-placeholder-shown:text-gray-400 peer-focus:-top-8 peer-focus:left-2'>
-              Email Address
-            </FormLabel>
-            <FormErrorMessage>Please enter a valid email.</FormErrorMessage>
-          </FormControl>
-        )}
-      />
+    <section className='flex flex-col items-center gap-8'>
+      <h1 className='self-center text-3xl font-medium'>Contact</h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className='flex w-full flex-col justify-center gap-12 md:w-3/4'
+      >
+        {/* register your input into the hook by invoking the "register" function */}
+        <Controller
+          name='name'
+          control={control}
+          rules={{ required: true }}
+          defaultValue=''
+          render={({ field }) => (
+            <FormControl isRequired isInvalid={errors.name ? true : false}>
+              <Input
+                {...field}
+                variant='outline'
+                placeholder='Name'
+                className='peer rounded-lg bg-slate-100 placeholder-transparent'
+              />
+              <FormLabel className='peer-focus:text-gray absolute -top-8 left-2 text-gray-400 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-8'>
+                Full Name
+              </FormLabel>
+              <FormErrorMessage>Please enter your name.</FormErrorMessage>
+            </FormControl>
+          )}
+        />
+        <Controller
+          name='email'
+          control={control}
+          defaultValue=''
+          rules={{
+            required: { value: true, message: 'Email Address is required.' },
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              message: 'Please input a valid email address.',
+            },
+          }}
+          render={({ field }) => (
+            <FormControl isRequired isInvalid={errors.email ? true : false}>
+              <Input
+                {...field}
+                variant='outline'
+                isInvalid={errors.email ? true : false}
+                errorBorderColor='crimson'
+                placeholder='Email Address'
+                className='peer rounded-lg bg-slate-100 placeholder-transparent'
+              />
+              <FormLabel className='absolute -top-8 left-2 text-gray-400 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:left-2 peer-placeholder-shown:text-gray-400 peer-focus:-top-8 peer-focus:left-2'>
+                Email Address
+              </FormLabel>
+              <FormErrorMessage>Please enter a valid email.</FormErrorMessage>
+            </FormControl>
+          )}
+        />
 
-      <Controller
-        name='message'
-        control={control}
-        rules={{ required: true }}
-        defaultValue=''
-        render={({ field }) => (
-          <FormControl isRequired isInvalid={errors.message ? true : false}>
-            <Textarea
-              {...field}
-              variant='outline'
-              placeholder='Send me a message!'
-              className=' peer rounded-lg bg-slate-100 placeholder-transparent'
-            />
-            <FormLabel className='absolute -top-8 left-2 text-gray-400 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:left-2 peer-placeholder-shown:text-gray-400 peer-focus:-top-8 peer-focus:left-2'>
-              Message
-            </FormLabel>
-            <FormErrorMessage>Please enter a message.</FormErrorMessage>
-          </FormControl>
-        )}
-      />
-      {/* include validation with required or other standard HTML validation rules */}
+        <Controller
+          name='message'
+          control={control}
+          rules={{ required: true }}
+          defaultValue=''
+          render={({ field }) => (
+            <FormControl isRequired isInvalid={errors.message ? true : false}>
+              <Textarea
+                {...field}
+                variant='outline'
+                placeholder='Send me a message!'
+                className=' peer rounded-lg bg-slate-100 placeholder-transparent'
+              />
+              <FormLabel className='absolute -top-8 left-2 text-gray-400 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:left-2 peer-placeholder-shown:text-gray-400 peer-focus:-top-8 peer-focus:left-2'>
+                Message
+              </FormLabel>
+              <FormErrorMessage>Please enter a message.</FormErrorMessage>
+            </FormControl>
+          )}
+        />
+        {/* include validation with required or other standard HTML validation rules */}
 
-      {/* errors will return when field validation fails  */}
-      {formClicked ? (
-        <p className='font-bold text-green-600'>
-          Thank you for submitting! You will be contacted shortly.
-        </p>
-      ) : (
-        <Button
-          type='submit'
-          className='border-2 border-solid hover:bg-green-600 hover:text-black'
-        >
-          Send me an email
-        </Button>
-      )}
-    </form>
+        {/* errors will return when field validation fails  */}
+        {isSuccess ? (
+          <p className='font-bold text-green-600'>{message}</p>
+        ) : (
+          <Button
+            type='submit'
+            className='border-2 border-solid hover:bg-green-600 hover:text-black'
+          >
+            {isSubmitting ? <Spinner /> : 'Send me an email'}
+          </Button>
+        )}
+      </form>
+    </section>
   )
 }
--- src/app/modules/hobbies/Hobbies.tsx
+++ src/app/modules/hobbies/Hobbies.tsx
@@ -8,7 +8,7 @@ export default function Hobbies({}: Props) {
       className='flex flex-col items-center justify-center gap-4 py-10 px-10'
       id='hobbies'
     >
-      <h2 className='mb-3 text-center text-3xl font-medium '>hobbies</h2>
+      <h2 className='mb-3 text-center text-3xl font-medium '>Hobbies</h2>
       <p>
         I have a passion for music, and I've made a few EDM mixes. You can find
         them below : )
